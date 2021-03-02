8758b0ca612eda9fe500f61904dc345b36c63d92
Jerson Carin <63774442+jersoncarin@users.noreply.github.com>
2021-03-02 12:40:50 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,12 +11,6 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
